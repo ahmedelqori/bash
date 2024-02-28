0227c4b770cb3c3902d0abe3f75117ff9bf83de9
ahmedelqori <aelqori1@gmail.com>
2024-02-28 13:42:37 +0100

    Fix Some execution Part

--- src/execution/ft_handle_child.c
+++ src/execution/ft_handle_child.c
@@ -6,7 +6,7 @@
 /*   By: meedivo <meedivo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/28 13:20:18 by meedivo           #+#    #+#             */
-/*   Updated: 2024/02/28 13:37:44 by meedivo          ###   ########.fr       */
+/*   Updated: 2024/02/28 13:41:05 by meedivo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,8 @@ void    ft_handle_child(t_list_pipe *lst, int *files , char **args, int *fd)
         dup2(files[1], STDOUT_FILENO);
     ft_close_all_files(fd,files);
 	if (execve(ft_strjoin("/bin/", args[0]), args, NULL) < 0)
+    {
         perror("ERROR:\t");
+        exit(1);
+    }
 }
